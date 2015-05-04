30de6f67bc0e7710c3f8bcde5c1c362c54ca4b64
Julien <julien@twenty-six-medias.com>
2015-05-04 09:46:22 -0400

    dialog done

--- angular/build/ng-pagevamp-ui.dev.latest.js
+++ angular/build/ng-pagevamp-ui.dev.latest.js
@@ -121,8 +121,18 @@ ngModule.directive('inPlace', function ($timeout) {
 */
 ngModule.directive('uiDialog', ['$compile', function ($compile) {
 	var component = function(scope, element, attrs, ctlr, transcludeFn) {
+		var $scope;
+		
 		// Get an instance of the scope
-		var $scope = window.$scope;
+		switch (attrs.ngScope) {
+			case "parent":
+				$scope = scope.$parent;
+				console.log("parent",scope.$parent);
+			break;
+			default:
+				$scope = scope.ngScope;
+			break;
+		}
 		
 		scope.safeApply = function(fn) {
 			var phase = this.$root.$$phase;
@@ -135,22 +145,20 @@ ngModule.directive('uiDialog', ['$compile', function ($compile) {
 			}
 		};
 		
-		// See this structure?
-		// Now look when I close the file and re-open it:
 		if (attrs.width) {
 			scope.width = attrs.width;
 		} else {
 			scope.width = 500;
 		}
 		
-		
+		/*
 		// Find the popup's ID
 		scope.popupId = attrs.uiDialog;
 		
 		scope.close = function() {
 			$scope.dialogs.hide(scope.popupId);
 		}
-		
+		*/
 		//$(element).html($compile($(element).html()));
 		
 		// Get the original content of the element
@@ -162,6 +170,34 @@ ngModule.directive('uiDialog', ['$compile', function ($compile) {
 		// Compile the html and inject into the element (replacing the original non-compiled content)
 		$(element).find('.dialog-content-placeholder').html($compile(cloned.html())($scope));
 		
+		
+		
+		scope.$watch("visible", function(a,b) {
+			if (scope.visible) {
+				$(element).fadeIn();
+			} else {
+				$(element).fadeOut();
+			}
+		});
+		
+		
+		/*
+		$scope.value = false;
+		
+		ngModelController.$render = function() {
+			$scope.safeApply(function() {
+				$scope.value = ngModelController.$viewValue || false;
+			});
+		};
+		
+		$scope.$watch("value", function(a,b) {
+			ngModelController.$setViewValue(a);
+		});
+		
+		*/
+		
+		
+		/*
 		// Monitor the display status fo the dialog
 		$scope.$watch('dialogs.display.'+scope.popupId, function() {
 			if ($scope.dialogs.display[scope.popupId]) {
@@ -170,7 +206,7 @@ ngModule.directive('uiDialog', ['$compile', function ($compile) {
 				$(element).fadeOut();
 			}
 		});
-		
+		*/
 		/*
 		// Monitor the popup width
 		$scope.$watch('dialogs.data.'+popupId+'.width', function() {
@@ -185,7 +221,11 @@ ngModule.directive('uiDialog', ['$compile', function ($compile) {
 		transclude:		true,
 		link: 			component,
 		replace:		false,
-		scope:			{},
+		//require:		"ngModel",
+		scope:			{
+			ngScope:	"=",
+			visible:	'='
+		},
 		templateUrl:	viewPath+'/view/dialog/dialog.html'
 	};
 }]);/*
